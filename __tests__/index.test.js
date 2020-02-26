3d1e03d9de9f798793608bf20df8e13eb4c57723
Mohamed IDRISSI <med.idrissi@outlook.com>
2020-02-26 23:41:50 +0100

    test: add dump test

---
+++ __tests__/index.test.js
@@ -0,0 +1,8 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+describe('dump test suite', () => {
+  it('dump test', async () => {
+    expect(true).to.eql(true);
+  });
+});
